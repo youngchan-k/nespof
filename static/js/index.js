eeeda31ad6c4be64b5081c669f5af8bc09dd2960
youngchan-k <kyc9618@gmail.com>
2023-12-12 18:36:34 +0900

    Update_12.12

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var NUM_INTERP_FRAMES = 55;
 
 var interp_images = [];
 function preloadInterpolationImages() {
